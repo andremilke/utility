fd69fbfb4c9fb08449c76227436caa25b382cc06
Andre Milke <andremilke@gmail.com>
2017-08-11 08:57:56 -0300

    Add files via upload

---
+++ CopyPasteXml.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+
+def CopyPasteXml(origem, destino):
+	for arquivo in os.listdir(origem):
+		path = os.path.join(origem, arquivo)
+		if os.path.isdir(path):
+			CopyPasteXml(path, destino)		
+		else:
+			extensao = os.path.splitext(arquivo)			
+			if(extensao[1] == '.xml'):
+				shutil.copyfile(os.path.join(origem, arquivo), os.path.join(destino, arquivo))				
+
+if __name__ == '__main__':
+	if len(sys.argv) == 3:    
+		CopyPasteXml(sys.argv[1], sys.argv[2])
+	else:
+		print("Necessario informar o path de Origem e de Destino")	
+	#CopyPasteXml('C:\\nfe\HistPython\\', 'C:\\nfe\\temp\\')
