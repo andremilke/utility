fdcfed6499285d49259b2d9937cffaa21af24f53
Andre Milke <andremilke@gmail.com>
2017-10-17 09:19:08 -0200

    Port Scan
    Port scan in python

---
+++ portscan.py
@@ -0,0 +1,10 @@
+import socket, sys
+
+if (len(sys.argv) <2):
+	print("How to use portscan")
+	print(sys.argv[0], "10.1.1.1")
+else:
+	for Port in range(1, 65535):
+		mysocket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		if (mysocket.connect_ex((sys.argv[1], Port)) == 0):
+			print("Port ", Port, "[OPENED]")	
