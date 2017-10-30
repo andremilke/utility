c13d21ea40cdf61196d5078344b234da62e42fd5
Andre Milke <andremilke@gmail.com>
2017-10-30 15:57:57 -0200

    Update portscan.py
    Adjustment PEP 8

--- portscan.py
+++ portscan.py
@@ -1,10 +1,12 @@
-import socket, sys
+import socket
+import sys
 
-if (len(sys.argv) <2):
-	print("How to use portscan")
-	print(sys.argv[0], "10.1.1.1")
+
+if (len(sys.argv) < 2):
+    print("How to use portscan")
+    print(sys.argv[0], "10.1.1.1")
 else:
-	for Port in range(1, 65535):
-		mysocket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		if (mysocket.connect_ex((sys.argv[1], Port)) == 0):
-			print("Port ", Port, "[OPENED]")	
+    for Port in range(1, 65535):
+        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if (mysocket.connect_ex((sys.argv[1], Port)) == 0):
+            print("Port ", Port, "[OPENED]")
